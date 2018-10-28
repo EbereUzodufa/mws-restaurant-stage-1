8feeef9a470d29974b7e9b541961983c9886c8ff
Ebere Uzodufa <ebereuzodufa@gmail.com>
2018-10-29 00:04:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -46,22 +46,6 @@ self.addEventListener('activate', function(event) {
 	);
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('mws-restaurant-') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
-
-
 self.addEventListener('fetch',function(event) {
   event.respondWith
   (    
